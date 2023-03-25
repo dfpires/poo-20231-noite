8edcabad3ff89ca8291dadf4e95d077a11a6c8ef
Daniel Pires <dfpires@gmail.com>
2023-03-24 21:59:32 -0300

    Add files via upload

---
+++ listas/src/lista4/heranca.ts
@@ -0,0 +1,12 @@
+class Animal {
+    // protegido para a filha herdar (pública) e as outras não (private)
+    protected name: string 
+}
+
+class Snake extends Animal {
+   
+}
+
+class Horse extends Animal {
+   
+}
