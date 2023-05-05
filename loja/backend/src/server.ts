706ee5c8029924ce85e7c2ce9266d235bcf67f5a
Daniel Pires <dfpires@gmail.com>
2023-05-05 20:41:28 -0300

    Add files via upload

--- loja/backend/src/server.ts
+++ loja/backend/src/server.ts
@@ -1,12 +1,13 @@
 // importa a dependência fastify
 import Fastify from 'fastify'
 // cria o objeto fastify
-const server = Fastify()
+const server = Fastify() // dependência para criar um servidor HTTP
 // importa a dependência prisma
 import {PrismaClient} from '@prisma/client'
 // cria o objeto prisma
-const prisma = new PrismaClient()
-
+const prisma = new PrismaClient() // ORM para conectar com SQLite
+// importa a dependência ZOD
+import {z} from 'zod'
 
 // vamos criar nossa primeira rota 
 // vamos usar o verbo GET - consulta
@@ -20,6 +21,29 @@ server.get('/products', async () => {
     // await aguarda a resposta do BD
     const products = await prisma.product.findMany()
     return products
+})
+
+// rota para consultar os produtos que iniciam a descrição com uma palavra
+// enviada pelo frontend (na variável request)
+server.get('/product/:description', async (request) => {
+    // precisamos utilizar um esquema de tipo de dados para tratar tipo recebido do usuário
+    // dependência ZOD
+    // criando o objeto ZOD para a descrição
+    const descriptionParam = z.object({
+        description: z.string()
+    })
+    // obtem o valor de description no parâmetro da rota
+    const {description} = descriptionParam.parse(request.params)
+    // faz a consulta no banco de dados
+    // select * from product where description = %description
+    const product = prisma.product.findMany({
+        where: {
+            description: {
+                startsWith: description
+            }
+        }
+    })
+    return product
 })
 
 // vamos subir o servidor
